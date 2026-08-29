05344400f77f435150bd409f1b9d1a43e36f39c3
QuJindai <129613167+QuJindai@users.noreply.github.com>
2026-08-29 03:34:51 -0700

    test(android): add diagnostic ui red gate

---
+++ android/knowledge-workbench/app/src/androidTest/java/com/mllm/knowledgeworkbench/MainActivityInstrumentedTest.java
@@ -0,0 +1,57 @@
+package com.mllm.knowledgeworkbench;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityInstrumentedTest {
+    @Test
+    public void diagnostic_screen_exposes_real_status_index_search_and_evidence_controls() {
+        try (ActivityScenario<MainActivity> ignored = ActivityScenario.launch(MainActivity.class)) {
+            onView(withId(R.id.status_lexical)).check(matches(isDisplayed()));
+            onView(withId(R.id.status_embedding)).check(matches(isDisplayed()));
+            onView(withId(R.id.status_hybrid)).check(matches(isDisplayed()));
+            onView(withId(R.id.text_database)).check(matches(isDisplayed()));
+            onView(withId(R.id.text_index_coverage)).check(matches(isDisplayed()));
+            onView(withId(R.id.button_import)).check(matches(isDisplayed()));
+            onView(withId(R.id.button_build_index)).check(matches(isDisplayed()));
+            onView(withId(R.id.progress_index)).check(matches(isDisplayed()));
+            onView(withId(R.id.text_index_progress)).check(matches(isDisplayed()));
+            onView(withId(R.id.input_query)).check(matches(isDisplayed()));
+            onView(withId(R.id.spinner_mode)).check(matches(isDisplayed()));
+            onView(withId(R.id.button_search)).check(matches(isDisplayed()));
+            onView(withId(R.id.results_container)).check(matches(isDisplayed()));
+            onView(withId(R.id.evidence_detail)).check(matches(isDisplayed()));
+        }
+    }
+
+    @Test
+    public void bundled_sample_can_build_vectors_and_return_hybrid_evidence() {
+        try (ActivityScenario<MainActivity> ignored = ActivityScenario.launch(MainActivity.class)) {
+            onView(withId(R.id.button_build_index)).perform(click());
+            onView(withId(R.id.status_hybrid)).check(matches(withText(containsString("可用"))));
+            onView(withId(R.id.text_index_coverage)).check(matches(withText(containsString("/"))));
+
+            onView(withId(R.id.input_query)).perform(replaceText("车辆制造"));
+            onView(withId(R.id.spinner_mode)).perform(click());
+            Espresso.onData(org.hamcrest.Matchers.is("HYBRID")).perform(click());
+            onView(withId(R.id.button_search)).perform(click());
+
+            onView(withId(R.id.results_container)).check(matches(isDisplayed()));
+            onView(withId(R.id.evidence_detail)).check(matches(withText(containsString("车辆"))));
+        }
+    }
+}
